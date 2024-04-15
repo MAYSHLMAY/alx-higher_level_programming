d93931bc7f8ec8473c0ee47e79edc0b87c3c6886
Mikiyas <educationforlife424@gmail.com>
2024-04-15 22:08:13 +0300

    cooked

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -6,6 +6,7 @@ def inherits_from(obj, a_class):
     '''the object is an instance of a class
     obj: an object
     a_class: a class
-    returns None
+    returns: True if the object is an instance
+		of a subclass, False otherwise.
     '''
     return (type(obj) != a_class and isinstance(obj, a_class))
