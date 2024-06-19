0d3ce82e72dc67f16addd7680e00fd4aed144997
Mikiyas Dawit <educationforlife424@gmail.com>
2024-06-19 20:07:17 +0300

    Fix eerno

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,30 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-const inputFile1 = process.argv[2];
-const inputFile2 = process.argv[3];
-const outputFile = process.argv[4];
-
-fs.readFile(inputFile1, 'utf8', (err1, data1) => {
-  if (err1) {
-    console.error(`Error reading file ${inputFile1}: ${err1}`);
-    return;
-  }
-
-  fs.readFile(inputFile2, 'utf8', (err2, data2) => {
-    if (err2) {
-      console.error(`Error reading file ${inputFile2}: ${err2}`);
-      return;
-    }
-
-    fs.writeFile(outputFile, data1 + data2, (err3) => {
-      if (err3) {
-        console.error(`Error writing file ${outputFile}: ${err3}`);
-        return;
-      }
-
-      console.log(`File ${outputFile} created successfully.`);
-    });
-  });
-});
+const fArg = fs.readFileSync(process.argv[2]).toString();
+const sArg = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fArg + '\n' + sArg);
