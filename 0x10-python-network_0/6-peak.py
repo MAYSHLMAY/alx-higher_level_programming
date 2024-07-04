85cd474344e3a4dfd8f24eed1704915faf7444d5
Mikiyas Dawit <educationforlife424@gmail.com>
2024-07-04 23:34:27 +0300

    done

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,8 +4,17 @@
 
 def find_peak(list_of_integers):
     """Finds a peak in list_of_integers"""
+    if (list_of_integers == []):
+        return None
     maxx = list_of_integers[0]
     for i in list_of_integers:
         if i > maxx:
             maxx = i
     return maxx
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
